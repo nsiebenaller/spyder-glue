b7731c8477bd950f6df26eb30681f9ae8f8333e5
Nicholas <nicholaspaulsiebenaller@gmail.com>
2024-06-10 13:26:12 -0400

    feat: github action deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	output: "export",
+	images: { unoptimized: true },
+	pageExtensions: ["ts", "tsx"],
+	basePath: isProd ? "/spyder-glue" : undefined,
+	assetPrefix: isProd ? "/spyder-glue" : undefined,
+};
 
 export default nextConfig;
