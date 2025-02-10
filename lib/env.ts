f869e9ce2ff38d536c1e22aa2ce85feaad4f7780
Nicholas <nicholaspaulsiebenaller@gmail.com>
2025-02-09 21:49:04 -0500

    fix: update public url to resolve for s3

--- lib/env.ts
+++ lib/env.ts
@@ -1,2 +1,3 @@
 export const NODE_ENV = String(process.env.NODE_ENV);
 export const IS_PRODUCTION = NODE_ENV === "production";
+export const IS_GITHUB_PAGES = Boolean(process.env.GITHUB_PAGES);
--- lib/publicURL.ts
+++ lib/publicURL.ts
@@ -1,10 +1,10 @@
-import { IS_PRODUCTION } from "./env";
+import { IS_GITHUB_PAGES } from "./env";
 
 const PREFIX = "spyder-glue";
 
 /** Formats a URL depending on the environment the app is running in. */
 export function publicURL(url: string): string {
-  if (IS_PRODUCTION) {
+  if (IS_GITHUB_PAGES) {
     return `/${[PREFIX, ...url.split("/").filter(Boolean)].join("/")}`;
   }
   return url;
